4f54d33b288c2e4d6a0743b0693066d0ec3960a8
RegardlessMan <1330292916@qq.com>
2024-08-26 22:47:42 +0800

    goroutine chan 简单使用

--- goroutine/main.go
+++ goroutine/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 var wg sync.WaitGroup
@@ -12,11 +13,24 @@ func hello(i int) {
 	fmt.Println("HELLO ", i)
 }
 
+func sendData(ch chan int) {
+	fmt.Println("Sending data...")
+	time.Sleep(5 * time.Second)
+	ch <- 42 // 发送操作将会阻塞，直到有人接收。
+	fmt.Println("Data sent!")
+}
+
+func receiveData(ch chan int) {
+	fmt.Println("Receiving data...")
+	<-ch // 接收操作将会阻塞，直到有数据可以接收。
+	fmt.Println("Data received!")
+}
+
 func main() {
-	fmt.Println("start")
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go hello(i)
-	}
-	wg.Wait()
+	ch := make(chan int) // 创建一个未缓冲的通道
+
+	go sendData(ch)
+	go receiveData(ch)
+
+	time.Sleep(10 * time.Second) // 让main goroutine等待一段时间，以确保其他goroutines完成执行。
 }
