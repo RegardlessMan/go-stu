a4d985c54cf7c1a83eb2c300e1e3d8b61e7b86b0
RegardlessMan <1330292916@qq.com>
2024-08-26 23:28:38 +0800

    goroutine chan 缓存和无缓冲通道简单 简单使用

--- goroutine/main.go
+++ goroutine/main.go
@@ -26,11 +26,51 @@ func receiveData(ch chan int) {
 	fmt.Println("Data received!")
 }
 
+func f2(c chan int) {
+	for {
+		v, ok := <-c
+		if !ok {
+			fmt.Println("chan status is end!")
+			break
+		}
+		fmt.Println("receive value is ", v, " chan status is ", ok)
+	}
+}
+
+func Produces() <-chan int {
+	c := make(chan int, 2)
+	go func() {
+		for i := 0; i <= 10; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+	return c
+}
+
+func Consumer(c <-chan int) {
+	for v := range c {
+		fmt.Println("consumer value ", v)
+	}
+}
+
 func main() {
-	ch := make(chan int) // 创建一个未缓冲的通道
+	//ch := make(chan int) // 创建一个未缓冲的通道
+	//go sendData(ch)
+	//go receiveData(ch)
+	//time.Sleep(10 * time.Second) // 让main goroutine等待一段时间，以确保其他goroutines完成执行。
 
-	go sendData(ch)
-	go receiveData(ch)
-
-	time.Sleep(10 * time.Second) // 让main goroutine等待一段时间，以确保其他goroutines完成执行。
+	//c := make(chan int, 6)
+	//c <- 10
+	//c <- 20
+	//c <- 30
+	//c <- 40
+	//c <- 50
+	//c <- 60
+	//go f2(c)
+	//time.Sleep(time.Second * 60)
+	//close(c)
+	c := Produces()
+	Consumer(c)
+	time.Sleep(time.Second * 10)
 }
