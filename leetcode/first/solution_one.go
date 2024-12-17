57950f100451ce409ccfca3773dc76c3c6f6e815
RegardlessMan <1330292916@qq.com>
2024-12-17 23:05:35 +0800

    删除有序数组中的重复项

--- leetcode/first/solution_one.go
+++ leetcode/first/solution_one.go
@@ -48,3 +48,22 @@ func removeElement(nums []int, val int) int {
 	}
 	return l
 }
+
+/*
+*
+删除有序数组中的重复项
+*/
+func removeDuplicates(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	l := 1
+	for i := 1; i < n; i++ {
+		if nums[i-1] != nums[i] {
+			nums[l] = nums[i]
+			l++
+		}
+	}
+	return l
+}
