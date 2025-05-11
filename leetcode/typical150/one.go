2dbb87f3258ab7a6c26cc381907dc8cb284f0bf7
qiegao <qiegao_ssj@foxmail.com>
2025-05-11 23:19:52 +0800

    合并两个数组

---
+++ leetcode/typical150/one.go
@@ -0,0 +1,22 @@
+package typical150
+
+// 合并两个数据
+// 解题思路
+// 1、从尾部开始填充
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	p1 := m - 1
+	p2 := n - 1
+	end := len(nums1) - 1
+
+	for p2 >= 0 {
+		// 如果 nums1 已经比较完，则直接拷贝 nums2 剩余部分
+		if p1 < 0 || nums2[p2] > nums1[p1] {
+			nums1[end] = nums2[p2]
+			p2--
+		} else {
+			nums1[end] = nums1[p1]
+			p1--
+		}
+		end--
+	}
+}
